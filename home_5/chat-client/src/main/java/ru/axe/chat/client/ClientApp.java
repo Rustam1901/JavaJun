08b4390fd6122b69a74c85d809e747c96a09198d
Rustam1901 <161497419+Rustam1901@users.noreply.github.com>
2025-02-24 19:00:33 +0300

    h_5

---
+++ home_5/chat-client/src/main/java/ru/axe/chat/client/ClientApp.java
@@ -0,0 +1,31 @@
+package ru.korostelev.chat.client;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientApp {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Start Client . . .");
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите ваш никнейм: ");
+            String name = scanner.nextLine();
+            Socket socket = new Socket("localhost", 8889);
+            Client client = new Client(socket, name);
+            InetAddress inetAddress = socket.getInetAddress();
+            System.out.println("InetAddress: " + inetAddress);
+            String remoteIp = inetAddress.getHostAddress();
+            System.out.println("RemoteIP: " + remoteIp);
+            System.out.println("LocalPort: " + socket.getLocalPort());
+
+            client.listenForMessage();
+            client.sendMessage();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ home_5/chat-server/src/main/java/ru/axe/chat/server/ServerApp.java
@@ -0,0 +1,17 @@
+package ru.korostelev.chat.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class ServerApp {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Start Server . . .");
+            ServerSocket serverSocket = new ServerSocket(8889);
+            Server server = new Server(serverSocket);
+            server.runServer();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
